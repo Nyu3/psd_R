c6c08a0614964801b3385ac2a36d544f4a1debb8
nishino <tateyokolr@gmail.com>
2020-10-01 11:49:21 +0900

    archive booster by SHIFT-JIS

--- PSD_archive_generator.R
+++ PSD_archive_generator.R
@@ -14,7 +14,7 @@
 
 
 tb. <- function(obj1, obj2, obj3, obj4) tibble(name = obj1, model = list(obj2), para = obj3, remark = obj4)
-arxiv <- bind_rows(  # Name, Model, Parameter number and Remark
+arxiv <-  'dplyr'::bind_rows(  # Name, Model, Parameter number and Remark
     tb.('Additive Weibull', addWeiF., 6, 'none'),
     tb.('Additive Weibull log-logistic', addWeiLogLogisF., 6, 'none'),
     tb.('Adjusted log-logistic generalized exponential', adjLogLogisGenExpF., 3, 'none'),
@@ -982,7 +982,7 @@ arxiv <- bind_rows(  # Name, Model, Parameter number and Remark
     tb.('4-parameter Kumaraswamy', kuma4F., 2, 'none'),
     tb.('4-parameter Topp-Leone Dagum', topLeoDag4F., 4, 'none')
 )
-arxiv <- mutate(arxiv, dist_No = row_number())
+arxiv <- 'dplyr'::mutate(arxiv, dist_No = row_number())
 
 
 ## Selection of PDF == (2020-06-21) ========================
